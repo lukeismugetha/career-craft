b2aedb553b2ef06cedafaad00bd1d6fcf3e6fe30
luke <92058197+lukeismugetha@users.noreply.github.com>
2025-10-22 10:43:01 +0300

    Connected backend and frontend, Tailwind fixed, backend running successfully

--- frontend/career-craft-frontend/src/App.jsx
+++ frontend/career-craft-frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import API from "./api";
+
 function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    API.get("/test")
+      .then((res) => setMessage(res.data.message))
+      .catch((err) => {
+        console.error("Error:", err);
+        setMessage("Error connecting to backend");
+      });
+  }, []);
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-600 text-white text-3xl font-bold">
-      TailwindCSS v4 is working 🎉
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">{message}</h1>
     </div>
   );
 }
